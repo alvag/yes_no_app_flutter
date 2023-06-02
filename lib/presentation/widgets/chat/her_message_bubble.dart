cca488be2c832d8ebedfbfca05498d9d31dde4e1
Max Alva Garcia <alva85@gmail.com>
2023-06-02 16:26:40 -0500

    Mostrar mensaje mientras carga la imagen

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -37,11 +37,20 @@ class _ImageBubble extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(20),
       child: Image.network(
-        'https://yesno.wtf/assets/no/23-5fe6c1ca6c78e7bf9a7cf43e406fb8db.gif',
-        width: size.width * 0.7,
-        height: 150,
-        fit: BoxFit.cover,
-      ),
+          'https://yesno.wtf/assets/no/23-5fe6c1ca6c78e7bf9a7cf43e406fb8db.gif',
+          width: size.width * 0.7,
+          height: 150,
+          fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) return child;
+
+            return Container(
+              width: size.width * 0.7,
+              height: 150,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: const Text('Esta cargando la imagen...'),
+            );
+          }),
     );
   }
 }
